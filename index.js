b00828eba05e80d49ddc1306af491a973763bd64
+ <+>
2021-08-11 19:53:31 -0700

    commit tree d154a76879a91ff622013d6f9e6d425542101804

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+function writeCards(stringNames, event){
+    let writeCardsNew = []
+    for(let i=0; i < stringNames.length; i++){
+        writeCardsNew.push(`Thank you, ${stringNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return writeCardsNew;
+}
+
 function countDown (firstNumber) {
     while (firstNumber > 0) {
         console.log(firstNumber);
         firstNumber -= 1;
     }
-    console.log(firstNumber)
+    console.log(firstNumber);
 }
-
-countDown(10);
